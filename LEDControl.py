8071c56bf27b3d3eadfb1f49812ce1024f5691a6
kamilkhalili <78993361+kamilkhalili@users.noreply.github.com>
2021-03-09 12:25:00 +0800

    Update LEDControl.py

--- LEDControl.py
+++ LEDControl.py
@@ -9,10 +9,10 @@ curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
-#Setup Serial communication
+# Setup Serial communication
 if __name__ == '__main__':
-#USB number might be different
-#Find out how to know the UART address
+# USB number might be different
+# Find out how to know the UART address
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) 
     ser.flush()
 
@@ -22,13 +22,14 @@ try:
             if char == ord('q'):
                 break
             elif char == ord('1'):
-                print "Toggle LED 1"
+                print "Toggle LED 1" # Write in RPi
+                ser.write(b"1\n") # Write in Arduino
             elif char == ord('2'):
                 print "Toggle LED 2"
+                ser.write(b"2\n")
             elif char == ord('3'):
                 print "Toggle LED 3"
-         
-             
+                ser.write(b"3\n")             
 finally:
     #Close down curses properly, inc turn echo back on!
     curses.nocbreak(); screen.keypad(0); curses.echo()
